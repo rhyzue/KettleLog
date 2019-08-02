7d99abeb619ddb39f169fd0bc0ddfb6df2245749
Rhyzue <Rennie6710@gmail.com>
2019-08-02 00:46:05 -0400

    Bring up blank primary stage with no functionality

--- PrimaryStage.java
+++ PrimaryStage.java
@@ -30,19 +30,41 @@ import javafx.scene.control.TableColumn.CellEditEvent;
 
 public class PrimaryStage extends Stage{
 
-	//VARIABLES AND NODES
-
+	//VARIABLES AND NODES -----------temp? Does this belong to main or here//if here, need get and possibly set methods
     double w_to_h = 1.4;
     double w = 1024;
     double h = w / w_to_h;
     double spacefromtable = 7.5;
 
-	Region opaqueLayer = new Region();
-	BorderPane base = new BorderPane();
+    double screenX = 0.0;
+    double screenY = 0.0; //----------uptohere
 
+    ObservableList<String> filterOptions = FXCollections.observableArrayList("Starred", "Most Recent", "Oldest Added", "None");
+
+
+	//Region opaqueLayer = new Region();
+	//BorderPane base = new BorderPane();
 	TextField searchbar = new TextField();
-
 	MenuBar kettlemenu = new MenuBar();
+    Kettlelog kettle = new Kettlelog();
+    //Handler eventHandler = new Handler();
+
+    public TableView<Item> table = new TableView<Item>();
+    String[] titles = {"", "Name","Status","Quantity","Minimum"};
+    TableColumn<Item, String>[] itemArray = (TableColumn<Item, String>[]) new TableColumn[titles.length];
+
+
+	PrimaryStage(BorderPane base, Region opaqueLayer){ //TEMP: opaque should belong to PS. make set method to access from main
+
+	 	this.setResizable(false);
+        this.setTitle("KettleLog");
+        /*
+        opaqueLayer.setStyle("-fx-background-color: #001a34;");
+        opaqueLayer.setOpacity(0.7);
+        opaqueLayer.setVisible(false); */
+
+        ////////////////////////////////////////////////////////////////Menu Bar
+        MenuBar kettlemenu = new MenuBar();
       
         //FILE SUBMENU
         Menu file = new Menu("File");
@@ -66,57 +88,76 @@ public class PrimaryStage extends Stage{
             MenuItem about = new Menu("About");
             MenuItem tutorial = new Menu("Tutorial");
             MenuItem credits = new Menu("Credits");
-
-        AnchorPane topBar = new AnchorPane();
-
-        Button addBtn = new Button();
-        Button removeBtn = new Button();
-
-		Handler eventHandler = new Handler();
         
-        FilteredList<Columns> filteredData = new FilteredList<>(data, p -> true);
-
-       
-        //FILTER COMBOBOX
-        ObservableList<String> filterOptions = FXCollections.observableArrayList("Starred", "Most Recent", "Oldest Added", "None");
-        ComboBox<String> filter= new ComboBox<String>(filterOptions);
-            
-        //KETTLELOG LOGO
-        Image kettleimage = new Image("./Misc/Logo.png");
-        ImageView logo = new ImageView();
-
-        VBox tableBox = new VBox(table);
-       
-        BorderPane main = new BorderPane();
-        
-        StackPane root = new StackPane();
-
-        Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
-
-	PrimaryStage(){
-
-	 	this.setResizable(false);
-        this.setTitle("KettleLog");
-
-		opaqueLayer.setStyle("-fx-background-color: #001a34;");
-        opaqueLayer.setOpacity(0.7);
-        opaqueLayer.setVisible(false);
-
-		file.getItems().addAll(newtable, opentable, printtable, exit);
+        //ADDING MENUITEMS TO THEIR RESPECTIVE MENUS
+        file.getItems().addAll(newtable, opentable, printtable, exit);
         edit.getItems().addAll(add, remove);
         view.getItems().addAll(fs, mi);
         info.getItems().addAll(about, tutorial, credits);
         kettlemenu.getMenus().addAll(file, edit, view, info);
+ 
 
+        //================================================================================
+        // TABLE
+        //================================================================================
+
+        table.setFixedCellSize(40.0);
+        table.setPrefSize(300, 508.0);
+        table.setPlaceholder(new Label("Sorry, your search did not match any item names."));
+
+        //COLUMN TITLES       
+        for(int i=0; i<titles.length; i++)
+        {
+            TableColumn<Item, String> item = new TableColumn<Item, String>(titles[i]);
+            item.setCellValueFactory(new PropertyValueFactory<>(titles[i].toLowerCase()));
+            item.prefWidthProperty().bind(table.widthProperty().multiply(0.1977));
+            item.setStyle( "-fx-alignment: CENTER-LEFT;");
+            item.setResizable(false);
+            itemArray[i] = item;
+        }
+
+        table.getColumns().<Item, String>addAll(itemArray);
+/*
+        ColumnHandler columnChange = new ColumnHandler(); //---------------maybe move later
+        table.getColumns().addListener(columnChange);  */
+
+        //table.setItems(data);  ------------------------------setTable method later
+
+        table.setOnMouseClicked((MouseEvent event) -> {
+            if (event.getClickCount() >= 1) {
+                System.out.println("PRINTING ROW INFORMATION BELOW.");
+                //onSelection();
+            }
+        });
+
+        VBox tableBox = new VBox(table);
+
+        //================================================================================
+        // TOP BAR (includes add/remove, filter, search)
+        //================================================================================
+
+        AnchorPane topBar = new AnchorPane();
+
+        //ADD BUTTON
+        Button addBtn = new Button();
         addBtn.setText("ADD");
         addBtn.setId("addBtn");
 
+        //REMOVE BUTTON
+        Button removeBtn = new Button();
+        removeBtn = new Button();
         removeBtn.setText("REMOVE");
         removeBtn.setId("removeBtn"); 
         removeBtn.setDisable(true);
 
+        //================================================================================
+        // ADD AND REMOVE BUTTON FUNCTIONALITY
+        //================================================================================
+        
+
+        /*Handler eventHandler = new Handler();  ----------------------add event handling later
         addBtn.setOnAction(eventHandler);
-        removeBtn.setOnAction(eventHandler);
+        removeBtn.setOnAction(eventHandler);*/
         
         //POSITIONS OF ADD AND REMOVE
         AnchorPane.setRightAnchor(addBtn, 135.0);
@@ -130,9 +171,12 @@ public class PrimaryStage extends Stage{
         AnchorPane.setLeftAnchor(searchbar, 305.0); 
         AnchorPane.setBottomAnchor(searchbar, spacefromtable);
 
-        searchbar.textProperty().addListener((observable, oldValue, newValue) -> {
+        /*
+        FilteredList<Columns> filteredData = new FilteredList<>(data, p -> true);  //---------------later
+
+        searchbar.textProperty().addListener((observable, oldValue, newValue) -> { //----------------move into class
             filteredData.setPredicate(Columns -> {
-                // If there is nothing in the search bar, we want to display all the items.
+                // If there is nothing in the search bar, we want to display all the items. 
                 if (newValue == null || newValue.isEmpty()) {
                     return true;
                 }
@@ -149,17 +193,23 @@ public class PrimaryStage extends Stage{
             CellGenerator cellFactory = new CellGenerator();    
             columns[0].setCellFactory(cellFactory);     
             table.setItems(sortedData);
-        });
-
-        filter.setPromptText("Filter By");
-        filter.setPrefWidth(150.0);
-
-        FilterHandler filterListener = new FilterHandler();
-        filter.valueProperty().addListener(filterListener);
+        }); */
+        
+        //FILTER COMBOBOX
+        ObservableList<String> filterOptions = FXCollections.observableArrayList("Starred", "Most Recent", "Oldest Added", "None");
+        ComboBox<String> filter= new ComboBox<String>(filterOptions);
+            filter.setPromptText("Filter By");
+            filter.setPrefWidth(150.0);
+/*
+            FilterHandler filterListener = new FilterHandler();
+            filter.valueProperty().addListener(filterListener);*/
 
         AnchorPane.setLeftAnchor(filter, 52.0);
         AnchorPane.setBottomAnchor(filter, spacefromtable);
 
+        //KETTLELOG LOGO
+        Image kettleimage = new Image("./Misc/Logo.png");
+        ImageView logo = new ImageView();
         logo.setFitHeight(100);
         logo.setFitWidth(200);
         logo.setImage(kettleimage);
@@ -171,7 +221,12 @@ public class PrimaryStage extends Stage{
         topBar.setStyle("-fx-background-color: #004080;");
         topBar.setPrefSize(100, 150);
         topBar.getChildren().addAll(logo, addBtn, removeBtn, searchbar, filter);
+        
+        //================================================================================
+        // FINALIZATION
+        //================================================================================
 
+        BorderPane main = new BorderPane();
         main.setStyle("-fx-background-color: #004080;");
         BorderPane.setMargin(tableBox, new Insets(10, 50, 0, 50));
         main.setCenter(tableBox);
@@ -180,20 +235,61 @@ public class PrimaryStage extends Stage{
         base.setTop(kettlemenu);
         base.setCenter(main);
 
+        StackPane root = new StackPane();
         root.getChildren().addAll(base, opaqueLayer);
 
         //SHOW SCENE (13 inch laptops are 1280 by 800)
 
+        this.setScene(new Scene(root, w, h));
+
+        Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
         screenX = (screenBounds.getWidth() - w) / 2;
         screenY = (screenBounds.getHeight() - h) / 2;
 
-        this.setScene(new Scene(root, w, h));
         this.setX(screenX); 
         this.setY(screenY);
-        this.show();
-
-        
     }
+/*
+    public class ColumnHandler implements ListChangeListener<TableColumn>{
+        public boolean suspended;
+
+            @Override
+            public void onChanged(Change change) {
+                change.next();
+                if (change.wasReplaced() && !suspended) {
+                    this.suspended = true;
+                    table.getColumns().setAll(columns);
+                    this.suspended = false;
+                }
+            }
+    }*/
+
+ /*   public class Handler implements EventHandler<ActionEvent>{
+        @Override
+        public void handle(ActionEvent e) {
+
+            String itemClicked = ((Control)e.getSource()).getId();
+
+            switch(itemClicked){
+                case "addBtn":
+                    presscount = 1;
+                    addItemPopup(0, emptyinfo, empty); 
+                    break;
+                case "removeBtn":
+                    for(int i=0; i<data.size(); i++){
+                        Columns curItem = data.get(i);
+                        if(curItem.getChecked()==true){
+                            System.out.println("Delete: "+ curItem.getName());
+                            itemsToDelete.add(curItem);
+                        }
+                    }
+                    displayAlert(itemsToDelete);
+                    break;    
+                default:
+                    System.out.println("Otherstuff");
+            }
+        }
+    }*/
 
 
 }
--- kettlelog.java
+++ kettlelog.java
@@ -35,8 +35,6 @@ public class Kettlelog extends Application {
     //================================================================================
     @SuppressWarnings("unchecked")
 
-    private static Stage setup;
-
     private ObservableList<Item> data; 
     private ObservableList<Item> itemsToDelete;
     private TableView<Item> table;
@@ -57,9 +55,19 @@ public class Kettlelog extends Application {
     double screenX = 0.0;
     double screenY = 0.0;
 
+    double w_to_h = 1.4;
+    double w = 1024;
+    double h = w / w_to_h;
+    double spacefromtable = 7.5;
+
     public static Region opaqueLayer = new Region();
-    public InfoStage infoStage = new InfoStage();
-    public AddStage addStage = new AddStage();
+    //public InfoStage infoStage = new InfoStage();
+    //public AddStage addStage = new AddStage();
+    //public PrimaryStage primaryStage;
+
+    public BorderPane base = new BorderPane();
+
+    //FilteredList<Item> filteredData = new FilteredList<>(data, p -> true);
 
     public static void main(String[] args) {
         launch(args);
@@ -67,217 +75,19 @@ public class Kettlelog extends Application {
  
     @Override
     public void start(Stage setup) {
+    	setup = new PrimaryStage(base, opaqueLayer);
 
-        this.setup = setup;
-        opaqueLayer.setStyle("-fx-background-color: #001a34;");
-        opaqueLayer.setOpacity(0.7);
-        opaqueLayer.setVisible(false);
-        
+    	//make setup the primaryStage
+    	//setup = primaryStage;        
         data = FXCollections.observableArrayList(empty);
         itemsToDelete = FXCollections.observableArrayList(empty);
-        table = new TableView<Item>();
 
-        //Here is our leftmost column, the one with the anchorpane full of buttons.
-        TableColumn<Item, Boolean> buttoncolumn = new TableColumn<>("");
-
-        //ensuring that our cellfactory is only added for non-empty rows.
-        buttoncolumn.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Item, Boolean>, ObservableValue<Boolean>>() {
-              @Override public ObservableValue<Boolean> call(TableColumn.CellDataFeatures<Item, Boolean> features) {
-                return new SimpleBooleanProperty(features.getValue() != null);
-              }
-          });
-
-        buttoncolumn.setCellFactory(new Callback<TableColumn<Item, Boolean>, TableCell<Item, Boolean>>() {
-              @Override public TableCell<Item, Boolean> call(TableColumn<Item, Boolean> itemBooleanTableColumn) {
-                new AddButtonCell(table, data);
-              }
-          });
-                
-        //COLUMN TITLES
-        String[] titles = {"Name","Status","Quantity","Minimum"};
-
-        //array of table
-        TableColumn<Item, String>[] colarray = (TableColumn<Item, String>[])new TableColumn[titles.length];
-
-        for(int i=0; i<titles.length; i++)
-        {
-            TableColumn<Item, String> column = new TableColumn<Item, String>(titles[i]);
-            column.setCellValueFactory(new PropertyValueFactory<>(titles[i].toLowerCase()));
-            column.prefWidthProperty().bind(table.widthProperty().multiply(0.1977));
-            column.setStyle( "-fx-alignment: CENTER-LEFT;");
-            column.setResizable(false);
-            colarray[i] = column;
-        }
-
-        table.getColumns().<Item, String>addAll(buttoncolumn, colarray);
-        table.setFixedCellSize(40.0);
-        table.setPrefSize(300, 508.0);
-        table.setPlaceholder(new Label("Sorry, your search did not match any item names."));
-
-        ColumnHandler columnChange = new ColumnHandler();
-        table.getColumns().addListener(columnChange); 
-        table.setItems(data);
-
-        VBox tableBox = new VBox(table);
-
-        //================================================================================
-        // MENU BAR
-        //================================================================================
-
-        MenuBar kettlemenu = new MenuBar();
-      
-        //FILE SUBMENU
-        Menu file = new Menu("File");
-            MenuItem newtable = new Menu("New");
-            MenuItem opentable = new Menu("Open");
-            MenuItem printtable = new Menu("Print");
-            MenuItem exit = new Menu("Exit");
-
-        //EDIT SUBMENU
-        Menu edit = new Menu("Edit");
-            MenuItem add = new Menu("Add Item");
-            MenuItem remove = new Menu("Remove Item");
-
-        //VIEW SUBMENU
-        Menu view = new Menu("View");
-            MenuItem fs = new Menu("Fullscreen");
-            MenuItem mi = new Menu("Minimal Interface");
-
-        //INFO SUBMENU
-        Menu info = new Menu("Info");
-            MenuItem about = new Menu("About");
-            MenuItem tutorial = new Menu("Tutorial");
-            MenuItem credits = new Menu("Credits");
-        
-        //ADDING MENUITEMS TO THEIR RESPECTIVE MENUS
-        file.getItems().addAll(newtable, opentable, printtable, exit);
-        edit.getItems().addAll(add, remove);
-        view.getItems().addAll(fs, mi);
-        info.getItems().addAll(about, tutorial, credits);
-        kettlemenu.getMenus().addAll(file, edit, view, info);
- 
-
-        //================================================================================
-        // TOP BAR (includes add/remove, filter, search)
-        //================================================================================
-
-        AnchorPane topBar = new AnchorPane();
-
-        //ADD BUTTON
-        Button addBtn = new Button();
-        addBtn.setText("ADD");
-        addBtn.setId("addBtn");
-
-        //REMOVE BUTTON
-        //Button removeBtn = new Button();
-        removeBtn = new Button();
-        removeBtn.setText("REMOVE");
-        removeBtn.setId("removeBtn"); 
-        removeBtn.setDisable(true);
-
-        //================================================================================
-        // ADD AND REMOVE BUTTON FUNCTIONALITY
-        //================================================================================
-        Handler eventHandler = new Handler();
-        addBtn.setOnAction(eventHandler);
-        removeBtn.setOnAction(eventHandler);
-        
-        //POSITIONS OF ADD AND REMOVE
-        AnchorPane.setRightAnchor(addBtn, 135.0);
-        AnchorPane.setBottomAnchor(addBtn, spacefromtable);
-        AnchorPane.setRightAnchor(removeBtn, 55.0);
-        AnchorPane.setBottomAnchor(removeBtn, spacefromtable);
-       
-        //SEARCH BAR
-        searchbar.setPrefWidth(w / 2.56);
-        searchbar.setPromptText("Search");
-        AnchorPane.setLeftAnchor(searchbar, 305.0); 
-        AnchorPane.setBottomAnchor(searchbar, spacefromtable);
-
-        FilteredList<Columns> filteredData = new FilteredList<>(data, p -> true);
-
-        searchbar.textProperty().addListener((observable, oldValue, newValue) -> {
-            filteredData.setPredicate(Columns -> {
-                // If there is nothing in the search bar, we want to display all the items.
-                if (newValue == null || newValue.isEmpty()) {
-                    return true;
-                }
-                // lowercasesearch is referring to what the user is searching in the search bar
-                String lowercasesearch = newValue.toLowerCase();
-                if (String.valueOf(Columns.getName()).toLowerCase().contains(lowercasesearch)) {
-                    return true;
-                } else {
-                    return false;
-                }
-            });
-            SortedList<Columns> sortedData = new SortedList<>(filteredData);
-            sortedData.comparatorProperty().bind(table.comparatorProperty());
-            /*CellGenerator cellFactory = new CellGenerator();    
-            columns[0].setCellFactory(cellFactory);*/    
-            table.setItems(sortedData);
-        });
-        
-        //FILTER COMBOBOX
-        ObservableList<String> filterOptions = FXCollections.observableArrayList("Starred", "Most Recent", "Oldest Added", "None");
-        ComboBox<String> filter= new ComboBox<String>(filterOptions);
-            filter.setPromptText("Filter By");
-            filter.setPrefWidth(150.0);
-
-            FilterHandler filterListener = new FilterHandler();
-            filter.valueProperty().addListener(filterListener);
-
-        AnchorPane.setLeftAnchor(filter, 52.0);
-        AnchorPane.setBottomAnchor(filter, spacefromtable);
-
-        //KETTLELOG LOGO
-        Image kettleimage = new Image("./Misc/Logo.png");
-        ImageView logo = new ImageView();
-        logo.setFitHeight(100);
-        logo.setFitWidth(200);
-        logo.setImage(kettleimage);
-
-        AnchorPane.setLeftAnchor(logo, 50.0);
-        AnchorPane.setTopAnchor(logo, 10.0);
-
-        //TOPBAR GENERAL INFORMATION 
-        topBar.setStyle("-fx-background-color: #004080;");
-        topBar.setPrefSize(100, 150);
-        topBar.getChildren().addAll(logo, addBtn, removeBtn, searchbar, filter);
-        
-        //================================================================================
-        // FINALIZATION
-        //================================================================================
-
-        BorderPane main = new BorderPane();
-        main.setStyle("-fx-background-color: #004080;");
-        BorderPane.setMargin(tableBox, new Insets(10, 50, 0, 50));
-        main.setCenter(tableBox);
-        main.setTop(topBar);
-
-        base.setTop(kettlemenu);
-        base.setCenter(main);
-
-        StackPane root = new StackPane();
-        root.getChildren().addAll(base, opaqueLayer);
-
-        //SHOW SCENE (13 inch laptops are 1280 by 800)
-
-        setup.setScene(new Scene(root, w, h));
-        Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
-
-        screenX = (screenBounds.getWidth() - w) / 2;
-        screenY = (screenBounds.getHeight() - h) / 2;
-
-        setup.setResizable(false);
-        setup.setTitle("KettleLog");
-        setup.setX(screenX); 
-        setup.setY(screenY);
         setup.show();
-
+        
 
     }
    
-
+/*
     public void showInfoStage(Item rowInfo){
         opaqueLayer.setVisible(true); 
         Bounds sb = base.localToScreen(base.getBoundsInLocal());
@@ -285,7 +95,7 @@ public class Kettlelog extends Application {
         yBounds = sb.getMinY();
 
         //Here we pass in the row's information to updateinfostage.
-        infoStage.initOwner(Kettlelog.primary);
+        infoStage.initOwner(primaryStage);
         infoStage.updateInfoStage(xBounds, yBounds, w, h, rowInfo);
         infoStage.show();
     }
@@ -308,20 +118,20 @@ public class Kettlelog extends Application {
     public void hideAddStage(){
         opaqueLayer.setVisible(false);
         addStage.hide();
-    }
-
+    } */
+/*
     public class ColumnHandler implements ListChangeListener<TableColumn>{
         public boolean suspended;
 
-            @Override
-            public void onChanged(Change change) {
-                change.next();
-                if (change.wasReplaced() && !suspended) {
-                    this.suspended = true;
-                    table.getColumns().setAll(columns);
-                    this.suspended = false;
-                }
+        @Override
+        public void onChanged(Change change) {
+            change.next();
+            if (change.wasReplaced() && !suspended) {
+                this.suspended = true;
+                table.getColumns().setAll(columns);
+                this.suspended = false;
             }
-    }
+        }
+    }*/
 
 }
